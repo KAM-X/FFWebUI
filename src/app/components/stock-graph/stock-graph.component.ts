e4efb85422b1cd1b519d6c13b63259161f4f9eb4
Gediminas Šeibokas <gedisaiba@gmail.com>
2024-04-07 13:08:24 +0300

    Created component to pass the initial test #ID-37

---
+++ src/app/components/stock-graph/stock-graph.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-graph.component.html',
+})
+export class StockGraphComponent {
+
+}
