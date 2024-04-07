e1d1fdfed2160a9b48e7a7d8586895c67f500c72
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-04-07 15:26:14 +0300

    Initial coomit for checking component creation #ID-40

---
+++ src/app/components/period-buttons/period-buttons.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PeriodButtonsComponent } from './period-buttons.component';
+
+describe('PeriodButtonsComponent', () => {
+  let component: PeriodButtonsComponent;
+  let fixture: ComponentFixture<PeriodButtonsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PeriodButtonsComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PeriodButtonsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
