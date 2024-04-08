6c363e13b8fb6dfa48ac8eca9f9520fb4aa881d9
Joris_Dev <61516195+JorisSDev@users.noreply.github.com>
2024-04-08 15:13:33 +0300

    Added test for data input #ID-41

--- src/app/components/stock-legend/stock-legend.component.spec.ts
+++ src/app/components/stock-legend/stock-legend.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StockLegendComponent } from './stock-legend.component';
 
 describe('StockLegendComponent', () => {
@@ -9,9 +8,10 @@ describe('StockLegendComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [StockLegendComponent]
-    })
-    .compileComponents();
-    
+    }).compileComponents();
+  });
+
+   beforeEach(() => {
     fixture = TestBed.createComponent(StockLegendComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,23 @@ describe('StockLegendComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have correct default values', () => {
+    component['stock1Name'] = 'NVIDIA';
+    component['stock1Data1'] = '-280$(-1.44%) Past day';
+    component['stock1Data2'] = 'Market open: 2024-04-08';
+    
+    component['stock2Name'] = 'AMD';
+    component['stock2Data1'] = '+190$(+2.44%) Past day';
+    component['stock2Data2'] = 'Market open: 2024-04-08';
+
+    expect(component.stock1Name).toBe('NVIDIA');
+    expect(component.stock1Data1).toBe('-280$(-1.44%) Past day');
+    expect(component.stock1Data2).toBe('Market open: 2024-04-08');
+
+    expect(component.stock2Name).toBe('AMD');
+    expect(component.stock2Data1).toBe('+190$(+2.44%) Past day');
+    expect(component.stock2Data2).toBe('Market open: 2024-04-08');
   });
 });
