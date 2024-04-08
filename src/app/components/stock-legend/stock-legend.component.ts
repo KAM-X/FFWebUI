c895fe3ef53c6a6e5db87acbd17bba6ebdb1e9d1
Joris_Dev <61516195+JorisSDev@users.noreply.github.com>
2024-04-08 13:19:55 +0300

    Create inital implementation for stock legend component #ID-41

---
+++ src/app/components/stock-legend/stock-legend.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-legend',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-legend.component.html',
+  styleUrl: './stock-legend.component.css'
+})
+export class StockLegendComponent {
+
+}
