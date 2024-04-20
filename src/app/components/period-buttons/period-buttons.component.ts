d802c1046b67a2f38ae2cfc9035effdd55aa7e32
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-04-20 15:03:15 +0300

    Fetch and output stock data to console based on selected period in PeriodButtonsComponent #ID-47

--- src/app/components/period-buttons/period-buttons.component.ts
+++ src/app/components/period-buttons/period-buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { StockDataService } from '../../services/stock-data.service';
 
 export interface PeriodChangeEvent {
   period: string;
@@ -14,9 +15,43 @@ export class PeriodButtonsComponent {
   @Output() periodChange = new EventEmitter<PeriodChangeEvent>();
   currentSelected: string = '1D';
 
+  constructor(private stockDataService: StockDataService) {}
+
   onPeriodSelect(period: string) {
     this.currentSelected = period;
-    console.log(`Button clicked for period: ${period}`); 
+    console.log(`Button clicked for period: ${period}`);
     this.periodChange.emit({ period });
+
+    let startDatetime = new Date();
+    let endDatetime = new Date();
+
+    switch (period) {
+      case '1D':
+        startDatetime.setDate(startDatetime.getDate() - 1);
+        break;
+      case '7D':
+        startDatetime.setDate(startDatetime.getDate() - 7);
+        break;
+      case '1M':
+        startDatetime.setMonth(startDatetime.getMonth() - 1);
+        break;
+      case '6M':
+        startDatetime.setMonth(startDatetime.getMonth() - 6);
+        break;
+      case 'YTD':
+        startDatetime = new Date(new Date().getFullYear(), 0, 1);
+        break;
+      case '1Y':
+        startDatetime.setFullYear(startDatetime.getFullYear() - 1);
+        break;
+      case '5Y':
+        startDatetime.setFullYear(startDatetime.getFullYear() - 5);
+        break;
+    }
+
+    this.stockDataService.getStockData('AAPL', startDatetime, endDatetime)
+        .subscribe(data => {
+          console.log('Stock Data for period:', period, data);
+        });
   }
 }
